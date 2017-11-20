b474762aa9c13161faf55fd674147abdf206925d
Joyce Wong <wong.cy.joyce@gmail.com>
2017-11-20 14:08:55 +0000

    created events model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120135301) do
+ActiveRecord::Schema.define(version: 20171120140626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.string "location"
+    t.text "description"
+    t.integer "distance"
+    t.integer "price"
+    t.string "surface"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "runs", force: :cascade do |t|
     t.bigint "user_id"
@@ -53,5 +67,6 @@ ActiveRecord::Schema.define(version: 20171120135301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
   add_foreign_key "runs", "users"
 end
