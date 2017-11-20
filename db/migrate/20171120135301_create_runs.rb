798100d734daef209853061c5bff3b955be6f1df
Joyce Wong <wong.cy.joyce@gmail.com>
2017-11-20 14:00:52 +0000

    added columns to run model

--- db/migrate/20171120135301_create_runs.rb
+++ db/migrate/20171120135301_create_runs.rb
@@ -1,7 +1,14 @@
 class CreateRuns < ActiveRecord::Migration[5.1]
   def change
     create_table :runs do |t|
-
+      t.references :user, foreign_key: true
+      t.string :location
+      t.date :date
+      t.text :description
+      t.integer :distance
+      t.integer :capacity
+      t.string :photo
+      t.boolean :shared
       t.timestamps
     end
   end
