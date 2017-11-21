8540eca9278142f0f85b4a4131718fc971dc9375
Agnes Ching <agnesching19@gmail.com>
2017-11-21 15:04:50 +0000

    first version of navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121124513) do
+ActiveRecord::Schema.define(version: 20171121140058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,15 +106,15 @@ ActiveRecord::Schema.define(version: 20171121124513) do
     t.string "location"
     t.time "pace"
     t.string "schedule"
-    t.string "username"
-    t.string "strava_id"
-    t.string "power_id"
-    t.string "nike_id"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "username"
+    t.string "strava_id"
+    t.string "power_id"
+    t.string "nike_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nike_id"], name: "index_users_on_nike_id", unique: true
     t.index ["power_id"], name: "index_users_on_power_id", unique: true
