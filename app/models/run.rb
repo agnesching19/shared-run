95321d429a288745983c9412b1556b91fd7d489f
Joe Alexander <ja5664@gmail.com>
2017-11-21 12:47:59 +0000

    basic search by location only added

--- app/models/run.rb
+++ app/models/run.rb
@@ -10,4 +10,14 @@ class Run < ApplicationRecord
   # Enables geocoder
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
+
+  # Enables pgsearch
+  include PgSearch
+  pg_search_scope :global_search,
+    against: [ :location ]
+
+    # ,
+    # associated_against: {
+    #   runs: [ :date]
+    # }
 end
