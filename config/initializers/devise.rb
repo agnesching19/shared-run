62e7a63333d1131b9f356e7e639660437d9662ee
Agnes Ching <agnesching19@gmail.com>
2017-11-21 12:05:13 +0000

    facebook appliment first attempt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,8 +250,15 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "APP_ID", "APP_SECRET", scope: 'email', info_fields: 'email,name'
 
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
+                  scope: 'public_profile,email',
+                  info_fields: 'email,first_name,last_name,gender,birthday,location,picture',
+                  client_options: {
+                      site: 'https://graph.facebook.com/v2.11',
+                      authorize_url: "https://www.facebook.com/v2.11/dialog/oauth"
+                  }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions'}
-  root to: 'pages#home'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :events
   resources :runs do
@@ -10,5 +9,10 @@ Rails.application.routes.draw do
 
   resources :invites, only: [:index, :show, :edit, :update, :destroy]
   resources :reviews, only: [:show, :edit, :update, :destroy]
+
+  # devise_scope :user do
+    # delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    # get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  # end
 
 end
