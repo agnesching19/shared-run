ee7079342d7f8fe155260e42c60cb021b29f8f36
Agnes Ching <agnesching19@gmail.com>
2017-11-23 15:09:27 +0000

    new route for message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
-  before_action :set_message, :set_run
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_run, only: [:new, :create, :index]
 
   def index
     @messages = policy_scope(Message).order(updated_at: :desc)
@@ -12,8 +13,15 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
     authorize @message
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.run_id = @run.id
+    @message.user_id = current_user.id
+    authorize @message
     if @message.save
-      redirect_to run_message(@message)
+      redirect_to run_messages_path(@message)
     else
       render :new
     end
@@ -23,6 +31,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content)
+  end
+
+  def set_run
+    @run = Run.find(params[:run_id])
   end
 
   def set_message
