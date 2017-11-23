2fb8aaeff462f89364f32d6e1dfbda839141f8e7
Agnes Ching <agnesching19@gmail.com>
2017-11-23 13:55:43 +0000

    temporary event show page, map display needed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   validates :date, :time, :location, :distance, presence: true
   belongs_to :user
+
+  # Enables geocoder
+  geocoded_by :location
+  after_validation :geocode, if: :location_changed?
 end
