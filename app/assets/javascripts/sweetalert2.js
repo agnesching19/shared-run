5bea880c890301a25833c32faed665b890ad1c4f
Agnes Ching <agnesching19@gmail.com>
2017-11-24 07:57:08 +0000

    added delete function on sweetalert

--- app/assets/javascripts/sweetalert2.js
+++ app/assets/javascripts/sweetalert2.js
@@ -5,3 +5,23 @@ function sweet(){
     'success',
   )
 }
+
+function sweetDelete(){
+  swal({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then(function (result) {
+    if (result.value) {
+      swal(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+}
