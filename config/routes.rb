a224a28789d8ad58bc8694e23988ec41f55335f3
Agnes Ching <agnesching19@gmail.com>
2017-11-26 20:38:14 +0000

    added preference controller and relative views

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } do
     get "logout" => "devise/sessions#destroy"
+
   end
 
   as :user do
     get "users/profile", to: "devise/registrations#edit", as: :user_root
+    get "users/:id/preferences", to: "preferences#index", as: :preferences
     get "users/:id/preferences/new", to: "preferences#new", as: :new_preference
     post "users/:id/preferences", to: "preferences#create"
-    get "users/:id/preferences", to: "preferences#show", as: :preference
     get "users/:id/preferences/edit", to: "preferences#edit", as: :edit_preference
     patch "users/:id/preferences", to: "preferences#update"
   end
 
   resources :events
+
   resources :runs do
     resources :invites, only: [:new, :create]
     resources :reviews, only: [:index, :new, :create]
@@ -27,6 +29,7 @@ Rails.application.routes.draw do
   resources :bookings, only: [:destroy]
 
   root to: "pages#home"
+
   get "users/:id/dashboard", to: "users#dashboard", as: "dashboard"
   get "pages/about", to: "pages#about", as: "about"
 
