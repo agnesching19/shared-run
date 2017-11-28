d0a0824feab5f9fe7cdf6e7a193a5b4d6e686602
Joyce Wong <wong.cy.joyce@gmail.com>
2017-11-28 19:51:52 +0000

    removed redundant columns from review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128110958) do
+ActiveRecord::Schema.define(version: 20171128194934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20171128110958) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
     t.index ["run_id"], name: "index_messages_on_run_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -67,6 +68,17 @@ ActiveRecord::Schema.define(version: 20171128110958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_preferences_on_user_id"
+  end
+
+  create_table "read_marks", id: :serial, force: :cascade do |t|
+    t.string "readable_type"
+    t.integer "readable_id"
+    t.string "reader_type"
+    t.integer "reader_id"
+    t.datetime "timestamp"
+    t.index ["readable_type", "readable_id"], name: "index_read_marks_on_readable_type_and_readable_id"
+    t.index ["reader_id", "reader_type", "readable_type", "readable_id"], name: "read_marks_reader_readable_index", unique: true
+    t.index ["reader_type", "reader_id"], name: "index_read_marks_on_reader_type_and_reader_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -84,8 +96,6 @@ ActiveRecord::Schema.define(version: 20171128110958) do
     t.boolean "punctuality"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "reviewer_id"
-    t.index ["reviewer_id"], name: "index_reviews_on_reviewer_id"
     t.index ["run_id"], name: "index_reviews_on_run_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
