a2e680d19abf0c3a5010e3c1ccc17cba984fb403
Agnes Ching <agnesching19@gmail.com>
2017-11-28 08:24:54 +0000

    removed validations from review table

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :reviewer, class_name: "User"
   validates :punctuality, presence: true
-  validates :user_id, :run_id, :reviewer_id
+  # validates :user_id, :run_id, :reviewer_id
 end
