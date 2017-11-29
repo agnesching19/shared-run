1d6a3608f908602e953fa62b7585245c47930dd1
Agnes Ching <agnesching19@gmail.com>
2017-11-29 17:28:30 +0000

    added open gragh meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :sociability, :path, :bio, :location, :strava, :nike, :photo, :password, :password_confirmation])
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
