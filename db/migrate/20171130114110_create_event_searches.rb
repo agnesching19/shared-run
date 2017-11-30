e650e38db2442bb48c0ee5f3603b8a008299e9fa
Joe Alexander <ja5664@gmail.com>
2017-11-30 15:29:03 +0000

    events and run searches working

--- db/migrate/20171130114110_create_event_searches.rb
+++ db/migrate/20171130114110_create_event_searches.rb
@@ -1,3 +1,14 @@
-Unstaged changes after reset:
-M	db/migrate/20171130114110_create_event_searches.rb
-D	db/migrate/20171130114110_create_events_search.rb
+class CreateEventSearches < ActiveRecord::Migration[5.1]
+  def change
+    create_table :event_searches do |t|
+      t.references :user, foreign_key: true
+      t.string :location
+      t.float :proximity, default: 10.0
+      t.integer :run_distance
+      t.date :run_date
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
+    end
+  end
+end
